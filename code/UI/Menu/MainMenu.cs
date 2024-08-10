5e9d482566fa7886b60e98e1d5c948ca3a4ff38f
Matthew James <Quacky2200@hotmail.com>
2024-08-10 05:45:21 +0100

    New Menu and Countdown styles
    Lots of changes towards styling both menu and the countdown.
    
    The countdown may need altering in future, it's not perfect.
    
    Following was done:
    - New Countdown, lerping between colors, shaking / pulsing
    - New button shapes, positioning, shading
    - Moved repetitive CSS to imported menu stylesheet
    - Moved stats CSS into stats.scss
    - Moved the stats in Play to the Stats menu.
    - Added a 'Game Mode' scene
    - Changed Level Scene to show fake levels - all load first map
    - Title can sit above the container as a game / main header, or in
      .content
    - Move .button-container > .buttons
    - Utilise .container wrapper before .content
    - Changed MainMenu class to support new GameMode selection
    
    The GameMode needs to expanded upon.

--- code/UI/Menu/MainMenu.cs
+++ code/UI/Menu/MainMenu.cs
@@ -3,6 +3,7 @@ using Sandbox;
 using Sandbox.Citizen;
 using Sandbox.Services;
 using System;
+using System.ComponentModel.Design;
 using System.Text.Json;
 using static Sandbox.Gizmo;
 
@@ -10,7 +11,8 @@ using static Sandbox.Gizmo;
 public enum MenuState
 {
 	Main,
-	Play,
+	GameMode,
+	LevelSelect,
 	Settings,
 	Stats	
 }
@@ -19,7 +21,14 @@ public class MainMenu : Component
 {
 	public static MainMenu instance;
 
+	private PanelComponent _currentMenu = null;
+	public PanelComponent currentMenu
+	{
+		get => _currentMenu;
+	}
+
 	[Group("Screens"), Property] public MainMenuScreen mainMenuScreen { get; private set; }
+	[Group("Screens"), Property] public GameModeScreen gameModeScreen { get; private set; }
 	[Group("Screens"), Property] public LevelSelectScreen levelSelectScreen { get; private set; }
 	[Group("Screens"), Property] public SettingsScreen settingsScreen { get; private set; }
 	[Group("Screens"), Property] public StatsScreen statsScreen { get; private set; }
@@ -33,6 +42,7 @@ public class MainMenu : Component
 	protected override void OnAwake()
 	{
 		instance = this;
+		_currentMenu = mainMenuScreen;
 
 		base.OnAwake();
 
@@ -64,18 +74,55 @@ public class MainMenu : Component
 		isRefreshingLeaderboards = false;
 	}
 
+	protected override void OnEnabled()
+	{
+		base.OnEnabled();
+
+		_currentMenu = mainMenuScreen;
+	}
+
 	public void SetMenuState(MenuState state)
 	{ 
 		menuState = state;
+		PanelComponent selected = null;
 
-		mainMenuScreen.Enabled = state == MenuState.Main;
+		switch (state)
+		{
+			case MenuState.Main:
+				selected = mainMenuScreen;
+				break;
+			case MenuState.GameMode:
+				selected = gameModeScreen;
+				break;
+			case MenuState.LevelSelect:
+				selected = levelSelectScreen;
+				break;
+			case MenuState.Settings:
+				selected = settingsScreen;
+				break;
+			case MenuState.Stats:
+				selected = statsScreen;
+				GetLeaderboard();
+				break;
+		}
+
+		if (selected == null)
+		{
+			Log.Warning("Selected is null using MenuState " + state.ToString());
+		}
+
+		_currentMenu.Enabled = false;
+		selected.Enabled = true;
+		_currentMenu = selected;
+
+		/*mainMenuScreen.Enabled = state == MenuState.Main;
 		levelSelectScreen.Enabled = state == MenuState.Play;
 		settingsScreen.Enabled = state == MenuState.Settings;
 		statsScreen.Enabled = state == MenuState.Stats;
 
 		if (state == MenuState.Play)
 		{
-			GetLeaderboard();
-		}
+			
+		}*/
 	}
 }
